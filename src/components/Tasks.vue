6aa0674fbc0d5be8b1650be88689067e9df70235
Francisco Sifuentes <fsifuentesm@hotmail.com>
2021-10-18 16:25:30 -0500

    :skull: add Tasks component

---
+++ src/components/Tasks.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="counter" class="bg-success text-white">
+    <div class="row p-3">
+      <div class="col-2">
+        <i class="fas fa-dollar-sign fa-4x"></i>
+      </div>
+      <div class="col-10 text-right">
+        <h2>{{(task.message)}}</h2>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Tasks',
+  computed: {
+    task() {
+      return this.$store.getters.currentTask;
+    },
+  },
+  methods: {},
+};
+</script>
